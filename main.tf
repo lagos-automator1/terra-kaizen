412211a84f8fad25a8def20dfb4504bf4dc95ac4
lagos-automator1 <somoriboy@gmail.com>
2024-02-17 12:20:26 +0000

    adding variables

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@
 ##################################################################################
 
 provider "aws" {
-  #access_key = "ACCESS_KEY"
-  #secret_key = "SECRET_KEY"
-  region     = "eu-west-2"
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
+  region = var.aws_region
 }
 
 ##################################################################################
---
+++ variables.tf
@@ -0,0 +1,50 @@
+/* Commented out aws creds as using Shared Creds
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+  sensitive   = true
+}
+*/
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region to use for resources"
+  default     = "eu-west-2"
+}
+
+## Adds variables to make more dynamic
+ariable "enable_dns_hostnames" {
+  type        = bool
+  description = "Enable DNS hostnames in VPC"
+  default     = true
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Base CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_public_subnet1_cidr_block" {
+  type        = string
+  description = "CIDR Block for Subnet 1 in VPC"
+  default     = "10.0.0.0/24"
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  description = "Map a public IP address for Subnet instances"
+  default     = true
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Type for EC2 Instnace"
+  default     = "t3.micro"
+}
