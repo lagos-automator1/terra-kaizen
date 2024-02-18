51dd1b38f6368a5625bef2bf091febe95dedea6a
lagos-automator1 <somoriboy@gmail.com>
2024-02-18 00:33:44 +0000

    no change really

--- network.tf
+++ network.tf
@@ -3,9 +3,9 @@
 ##################################################################################
 
 provider "aws" {
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  #  access_key = var.aws_access_key
+  #  secret_key = var.aws_secret_key
+  region = var.aws_region
 }
 
 ##################################################################################
